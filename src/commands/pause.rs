d6c80f84d4389142cb45e03f3b1620ba5fa61602
SpongyBacon <spongybacon@hotmail.co.uk>
2017-09-23 17:35:32 +0100

    use AudioPlayer#pause for command

--- src/commands/pause.rs
+++ src/commands/pause.rs
@@ -1,13 +1,12 @@
 use keys;
 
-use lavalink::message;
 use serenity::client::Context;
 use serenity::framework::standard::Args;
 use serenity::model::*;
 
 fn toggle_paused(ctx: &mut Context, msg: &Message, pause: bool) -> Result<(), String> {
     let guild_id = match msg.guild_id() {
-        Some(guild_id) => guild_id.0.to_string(),
+        Some(guild_id) => guild_id.0,
         None => {
             println!("oh no! no guild id??");
             return Ok(());
@@ -16,9 +15,23 @@ fn toggle_paused(ctx: &mut Context, msg: &Message, pause: bool) -> Result<(), St
 
     {
         let data = ctx.data.lock();
-        let ws_tx = data.get::<keys::LavalinkSocketSender>().unwrap();
 
-        let _ = ws_tx.lock().unwrap().send(message::pause(&guild_id, pause));
+        let player_manager = data.get::<keys::LavalinkAudioPlayerManager>()
+            .expect("keys::LavalinkAudioPlayerManager not present in Context::data");
+
+        let player_manager = player_manager.lock()
+            .expect("could not obtain lock on player manager");
+
+        let player = match player_manager.get_player(&guild_id) {
+            Some(player) => player,
+            None => {
+                let _ = msg.channel_id.say("this guild does not have an audio player");
+                return Ok(());
+            }
+        };
+
+        player.lock().as_mut().map(|lock| lock.pause(pause))
+            .expect("error obtaining lock on player and pausing");
     }
 
     let _ = msg.channel_id.say(
