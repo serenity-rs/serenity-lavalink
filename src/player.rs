1f0b17d85b9352bd53bc93f3df08572d0e5d90aa
SpongyBacon <spongybacon@hotmail.co.uk>
2018-03-10 09:38:10 +0000

    player sender should be Arc<Mutex<Sender<OwnedMessage>>>

--- src/player.rs
+++ src/player.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 use lavalink::model::{Pause, Play, Stop, Volume};
 use ::prelude::*;
 use ::listener::AudioPlayerListener;
+use websocket::OwnedMessage;
 
 type AudioPlayerMap = HashMap<u64, Arc<Mutex<AudioPlayer>>>;
 
@@ -14,7 +15,7 @@ type AudioPlayerMap = HashMap<u64, Arc<Mutex<AudioPlayer>>>;
 // where mutablity should not be nessesary for non state fields
 #[derive(Clone)]
 pub struct AudioPlayer {
-    pub sender: Arc<Mutex<Sender<Vec<u8>>>>,
+    pub sender: Arc<Mutex<Sender<OwnedMessage>>>,
     pub guild_id: u64,
     pub track: Option<String>,
     pub time: i64,
@@ -25,7 +26,7 @@ pub struct AudioPlayer {
 }
 
 impl AudioPlayer {
-    fn new(sender: Arc<Mutex<Sender<Vec<u8>>>>, guild_id: u64, listener: Arc<AudioPlayerListener>) -> Self {
+    fn new(sender: Arc<Mutex<Sender<OwnedMessage>>>, guild_id: u64, listener: Arc<AudioPlayerListener>) -> Self {
         Self {
             sender,
             guild_id,
@@ -40,7 +41,7 @@ impl AudioPlayer {
 
     #[inline]
     fn send(&self, message: Vec<u8>) -> Result<()> {
-        self.sender.lock().send(message).map_err(From::from)
+        self.sender.lock().send(OwnedMessage::Binary(message)).map_err(From::from)
     }
 
     pub fn play(
@@ -177,7 +178,7 @@ impl AudioPlayerManager {
     }
 
     // utility assosiated function for creating AudioPlayer instances wrapped in Arc & Mutex
-    fn new_player(&self, sender: Arc<Mutex<Sender<Vec<u8>>>>, guild_id: u64) -> Arc<Mutex<AudioPlayer>> {
+    fn new_player(&self, sender: Arc<Mutex<Sender<OwnedMessage>>>, guild_id: u64) -> Arc<Mutex<AudioPlayer>> {
         Arc::new(Mutex::new(AudioPlayer::new(sender, guild_id, self.listener.clone())))
     }
 
@@ -194,7 +195,7 @@ impl AudioPlayerManager {
         Some(Arc::clone(player))
     }
 
-    pub fn create_player(&mut self, sender: Arc<Mutex<Sender<Vec<u8>>>>, guild_id: u64) -> Result<Arc<Mutex<AudioPlayer>>> {
+    pub fn create_player(&mut self, sender: Arc<Mutex<Sender<OwnedMessage>>>, guild_id: u64) -> Result<Arc<Mutex<AudioPlayer>>> {
         // we dont use #has_key yet because it would get its own players clone & mutex lock
         if self.players.contains_key(&guild_id) {
             return Err(Error::PlayerAlreadyExists);
